6c5eab32dd8da3850fcc8f1a018c3e4020dfc37f
Adam K <adam@donotemail.com>
2009-10-20 03:02:34 +0000

    v0.9.98 almost complete
    git-svn-id: https://android-smspopup.googlecode.com/svn/trunk@95 dacd1dde-df38-11dd-9759-139c9ab14fc6

--- SMSPopup/src/net/everythingandroid/smspopup/SmsMessageSender.java
+++ SMSPopup/src/net/everythingandroid/smspopup/SmsMessageSender.java
@@ -26,7 +26,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteException;
 import android.net.Uri;
-import android.telephony.gsm.SmsManager;
+import android.telephony.SmsManager;
 
 public class SmsMessageSender {
   private final Context mContext;
