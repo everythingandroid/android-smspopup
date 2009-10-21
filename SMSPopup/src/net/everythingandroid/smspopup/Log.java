275c3fe51bc70c6a04d09792d8085ece3ce69e3d
adam@everythingandroid.net <adam@everythingandroid.net@dacd1dde-df38-11dd-9759-139c9ab14fc6>
2009-10-21 04:01:09 +0000

    v1.0.0 RC1
    git-svn-id: https://android-smspopup.googlecode.com/svn/trunk@98 dacd1dde-df38-11dd-9759-139c9ab14fc6

--- SMSPopup/src/net/everythingandroid/smspopup/Log.java
+++ SMSPopup/src/net/everythingandroid/smspopup/Log.java
@@ -3,7 +3,7 @@ package net.everythingandroid.smspopup;
 public class Log {
   public final static String LOGTAG = "SMSPopup";
 
-  public static final boolean DEBUG = false;
+  public static final boolean DEBUG = true;
 
   public static void v(String msg) {
     android.util.Log.v(LOGTAG, msg);
