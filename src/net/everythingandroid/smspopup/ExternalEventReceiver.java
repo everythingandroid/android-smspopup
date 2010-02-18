d5812a0027314d7d26189e503133c8fe7586da31
adam@everythingandroid.net <adam@everythingandroid.net@dacd1dde-df38-11dd-9759-139c9ab14fc6>
2010-02-18 02:58:52 +0000

    -add support for docked state (use regular notifications)
    git-svn-id: https://android-smspopup.googlecode.com/svn/trunk/SMSPopup@243 dacd1dde-df38-11dd-9759-139c9ab14fc6

--- src/net/everythingandroid/smspopup/ExternalEventReceiver.java
+++ src/net/everythingandroid/smspopup/ExternalEventReceiver.java
@@ -10,6 +10,11 @@ public class ExternalEventReceiver extends BroadcastReceiver {
   public static final String ACTION_SMSPOPUP_ENABLE = "net.everythingandroid.smspopup.ENABLE";
   public static final String ACTION_SMSPOPUP_DISABLE = "net.everythingandroid.smspopup.DISABLE";
   public static final String ACTION_SMSPOPUP_DONATED = "net.everythingandroid.smspopup.DONATED";
+  public static final String ACTION_DOCK_EVENT = "android.intent.action.DOCK_EVENT";
+  public static final String EXTRA_DOCK_STATE = "android.intent.extra.DOCK_STATE";
+  public static final int EXTRA_DOCK_STATE_UNDOCKED = 0;
+  public static final int EXTRA_DOCK_STATE_DESK = 1;
+  public static final int EXTRA_DOCK_STATE_CAR = 2;
 
   @Override
   public void onReceive(Context context, Intent intent) {
@@ -26,6 +31,30 @@ public class ExternalEventReceiver extends BroadcastReceiver {
         PreferenceManager.getDefaultSharedPreferences(context).edit();
       settings.putBoolean(context.getString(R.string.pref_donated_key), true);
       settings.commit();
+    } else if (ACTION_DOCK_EVENT.equals(action)) {
+
+      SharedPreferences.Editor settings =
+        PreferenceManager.getDefaultSharedPreferences(context).edit();
+
+      int event = intent.getIntExtra(EXTRA_DOCK_STATE, -1);
+      settings.putInt(context.getString(R.string.pref_docked_key), event);
+      settings.commit();
+
+      //      switch (event) {
+      //        case EXTRA_DOCK_STATE_UNDOCKED:
+      //          if (Log.DEBUG) Log.v("Phone was undocked!");
+      //          settings.putBoolean("docked", false);
+      //
+      //          break;
+      //        case EXTRA_DOCK_STATE_DESK:
+      //          if (Log.DEBUG) Log.v("Phone was docked to desk!");
+      //          break;
+      //        case EXTRA_DOCK_STATE_CAR:
+      //          if (Log.DEBUG) Log.v("Phone was docked to car!");
+      //          break;
+      //      }
+
+
     }
   }
 }
