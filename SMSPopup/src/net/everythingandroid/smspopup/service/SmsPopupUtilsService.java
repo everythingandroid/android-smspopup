ee6443ad273ae3070f00e40452bb7ee0501c399e
adam.everythingandroid@gmail.com <adam.everythingandroid@gmail.com@dacd1dde-df38-11dd-9759-139c9ab14fc6>
2012-03-05 04:40:33 +0000

    clean up default prefs (string -> bool) clean up contact notifications activity (use loaders for both adapters) auto show/hide keyboard for quick message activity
    git-svn-id: https://android-smspopup.googlecode.com/svn/trunk@365 dacd1dde-df38-11dd-9759-139c9ab14fc6

--- SMSPopup/src/net/everythingandroid/smspopup/service/SmsPopupUtilsService.java
+++ SMSPopup/src/net/everythingandroid/smspopup/service/SmsPopupUtilsService.java
@@ -1,10 +1,15 @@
 package net.everythingandroid.smspopup.service;
 
 import net.everythingandroid.smspopup.provider.SmsMmsMessage;
+import net.everythingandroid.smspopup.provider.SmsPopupContract.ContactNotifications;
 import net.everythingandroid.smspopup.util.Log;
 import net.everythingandroid.smspopup.util.ManageNotification;
 import net.everythingandroid.smspopup.util.SmsPopupUtils;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 
 import com.commonsware.cwac.wakeful.WakefulIntentService;
 
@@ -25,7 +30,10 @@ public class SmsPopupUtilsService extends WakefulIntentService {
 
     public static final String ACTION_QUICKREPLY =
             "net.everythingandroid.smspopup.ACTION_QUICKREPLY";
-
+    
+    public static final String ACTION_SYNC_CONTACT_NAMES =
+            "net.everythingandroid.smspopup.ACTION_SYNC_CONTACT_NAMES";
+    
     public SmsPopupUtilsService() {
         super(TAG);
     }
@@ -63,8 +71,75 @@ public class SmsPopupUtilsService extends WakefulIntentService {
         } else if (ACTION_UPDATE_NOTIFICATION.equals(action)) {
             if (Log.DEBUG) Log.v("SMSPopupUtilsService: Updating notification");
             updateNotification(intent);
+        } else if (ACTION_SYNC_CONTACT_NAMES.equals(action)) {
+        	if (Log.DEBUG) Log.v("SMSPopupUtilsService: Sync'ing contact names");
+        	syncContactNames(this);
+        	
         }
-    }    
+    }
+    
+    /**
+     * Any custom contact notifications are stored in a local database, including the contact names
+     * so we can quickly display them on the configuration screens. This function will loop through
+     * the locally stored contacts and check to see if the system contact name has changed at all
+     * (from either a manual edit or some sort of sync event). If so, it will update the local
+     * database with the new name.
+     * @param context Context.
+     * @return The number of rows updated with a new name.
+     */
+    private int syncContactNames(Context context) {
+    	
+    	final ContentResolver contentResolver = context.getContentResolver();
+        final Cursor cursor = contentResolver.query(
+                ContactNotifications.CONTENT_URI, null, null, null, null);
+        
+        if (cursor == null) {
+        	return 0;
+        }
+        
+        if (cursor.getCount() == 0) {
+        	return 0;
+        }
+        
+        int count = 0;
+        int updatedCount = 0;
+        String contactId;
+        String contactName;
+        String contactLookup;
+        String sysContactName;
+
+        // loop through the local sms popup contact notifications table
+        while (cursor.moveToNext()) {
+            count++;
+
+            contactName = cursor.getString(
+                    cursor.getColumnIndexOrThrow(ContactNotifications.CONTACT_NAME));
+            contactId = cursor.getString(
+                    cursor.getColumnIndexOrThrow(ContactNotifications._ID));
+            contactLookup = cursor.getString(
+                    cursor.getColumnIndexOrThrow(ContactNotifications.CONTACT_LOOKUPKEY));
+
+            sysContactName = SmsPopupUtils.getPersonNameByLookup(context, contactLookup);
+            
+            if (sysContactName != null && !sysContactName.equals(contactName)) {
+            	ContentValues vals = new ContentValues();
+            	vals.put(ContactNotifications.CONTACT_NAME, sysContactName);
+            	if (1 == contentResolver.update(
+            			ContactNotifications.buildContactUri(contactId), vals, null, null)) {
+            		updatedCount++;
+            	}
+            }
+        }
+        
+        if (cursor != null) {
+        	cursor.close();
+        }
+        
+        if (Log.DEBUG)
+        	Log.v("Sync Contacts: " + updatedCount + " / " + count);
+        
+        return updatedCount;
+    }
 
     private void updateNotification(Intent intent) {
         // In the case the user is "replying" to the message (ie. starting an
